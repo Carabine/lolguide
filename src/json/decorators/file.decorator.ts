f5faeb853b476968cd3b8bfe129114dfa0145160
unknown <busystrike@gmail.com>
2024-05-06 23:42:33 +0300

    rm decorator

--- src/json/decorators/file.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-
-export const FileDecorator = createParamDecorator(
-  async (data, context: ExecutionContext) => {
-    const request = context.switchToHttp().getRequest();
-    const file = await request.file();
-    return file;
-  },
-);
